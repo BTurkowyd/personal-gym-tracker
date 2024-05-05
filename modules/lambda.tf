65a53115af63b09637074fe1e93b7a74eb887b9c
Bartosz Turkowyd <vertax@gmail.com>
2024-05-05 16:51:19 +0200

    adding an API gateway

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -47,3 +47,12 @@ data "archive_file" "recent_workouts" {
   source_file = "${path.module}/src/load_recent_workouts.py"
   output_path = "${path.module}/src/load_recent_workouts.zip"
 }
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fetch_recent_from_hevy.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.silka_workouts.execution_arn}/*/*"
+}
