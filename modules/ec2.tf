f080e18d51b20cec9a8801ccd6343cde8d78700b
Bartosz Turkowyd <vertax@gmail.com>
2024-05-10 14:38:17 +0200

    postgres installed on EC2 as a docker container

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,32 +1,22 @@
-# resource "aws_instance" "postgres" {
-#   ami = "ami-01e444924a2233b07"
-#   instance_type = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.ec2_pg_sg.id]
-#   associate_public_ip_address = true
-#   subnet_id = aws_subnet.public_a.id
-#
-#   user_data = <<-EOF
-#     #!/bin/bash
-#     sudo apt update
-#     apt install -y postgresql postgresql-contrib
-#
-#     sudo service postgresql start
-#     sleep 5
-#
-#     sudo -u postgres psql -c "CREATE DATABASE testdb;"
-#     sudo -u postgres psql -c "CREATE USER ${local.envs["DB_USERNAME"]} WITH ENCRYPTED PASSWORD '${local.envs["DB_PASSWORD"]}';"
-#     sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE testdb TO ${local.envs["DB_USERNAME"]};"
-#
-#     echo "listen_addresses = '*'          # what IP address(es) to listen on;" >> /etc/postgresql/16/main/postgresql.conf
-#     echo "host    all    all    0.0.0.0/0   md5" >> /etc/postgresql/16/main/pg_hba.conf
-#     echo "host    all    all    ::/0        md5" >> /etc/postgresql/16/main/pg_hba.conf
-#
-#     sudo systemctl restart postgresql
-#
-#
-#   EOF
-#
-#   tags = {
-#     Name = "WorkoutsPostgresDB"
-#   }
-# }
+resource "aws_instance" "postgres" {
+  ami = "ami-01e444924a2233b07"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_pg_sg.id]
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.public_a.id
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y docker.io docker-compose
+    sudo systemctl start docker
+    sudo systemctl enable docker
+
+    sudo docker pull postgres:alpine
+    sudo docker run --name my-postgres -e POSTGRES_PASSWORD=${local.envs["DB_PASSWORD"]} -p 5432:5432 -d postgres:alpine
+  EOF
+
+  tags = {
+    Name = "WorkoutsPostgresDB"
+  }
+}
