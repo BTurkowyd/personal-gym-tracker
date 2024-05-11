557c95decdc1801ceb1ebca6ab6039ecec77d4a8
Bartosz Turkowyd <vertax@gmail.com>
2024-05-11 10:27:50 +0200

    setting the ec2 instance to t3.nano

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "postgres" {
   ami = "ami-01e444924a2233b07"
-  instance_type = "t4g.nano"
+  instance_type = "t3.nano"
   vpc_security_group_ids = [aws_security_group.ec2_pg_sg.id]
   associate_public_ip_address = true
   subnet_id = aws_subnet.public_a.id
