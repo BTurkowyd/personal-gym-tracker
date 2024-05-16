64126c7e7667caacea22b524080020ba3ec39be8
Bartosz Turkowyd <vertax@gmail.com>
2024-05-16 18:26:32 +0200

    cleaning

--- modules/locals.tf
+++ modules/locals.tf
@@ -1,11 +1,3 @@
 locals {
   envs = { for tuple in regexall("(.*?)=(.*)", file(var.env_file)) : tuple[0] => tuple[1] }
 }
-
-data "external" "get_git_branch" {
-  program = ["bash", "${path.module}/src/get-branch.sh"]
-}
-
-output "branch_name" {
-  value = sha1(data.external.get_git_branch.result["branch"])
-}
