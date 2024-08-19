1811561bf929de5b0efadaa87099dc979cfce194
Bartosz Turkowyd <vertax@gmail.com>
2024-08-19 21:50:02 +0200

    #56 preparing EC2 for superset

--- modules/superset_instance/sg.tf
+++ modules/superset_instance/sg.tf
@@ -13,11 +13,19 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 }
 
 # This ingres rule limits the access only from my own IP (which is dynamic, thus it is not hardcoded)
-resource "aws_vpc_security_group_ingress_rule" "postgres_port" {
+resource "aws_vpc_security_group_ingress_rule" "http_port" {
   ip_protocol       = "tcp"
   security_group_id = aws_security_group.ec2_pg_sg.id
-  from_port = 5432
-  to_port = 5432
+  from_port = 80
+  to_port = 80
+  cidr_ipv4 = "${chomp(data.http.myip.response_body)}/32"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https_port" {
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.ec2_pg_sg.id
+  from_port = 443
+  to_port = 443
   cidr_ipv4 = "${chomp(data.http.myip.response_body)}/32"
 }
 
